0de9ed9cab8ea70767441571ca60cc7ad0dab698
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-01-09 18:10:26 -0600

    added column function

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let toDos = function () {
+
+        if (window.localStorage.getItem("toDos") === null) {
+            return [
+                {title: "No To-Dos"}
+            ];
+        } else {
+            return JSON.parse(window.localStorage.getItem("toDos"));
+        }
+    }();
+
+
+let list = document.getElementById('to-do-row');
+list.innerHTML = '<div class= "col-lg-6 mx-auto item">' + toDos[0].title + '</div>'
