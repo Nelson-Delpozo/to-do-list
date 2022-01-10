3bbdddcc260455147a5989cc58a69440d97e4eb7
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-01-09 21:34:17 -0600

    working on functions

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -4,7 +4,7 @@ let toDos = function () {
     if (window.localStorage.getItem("toDos") === null) {
         return [
             {title: "No To-Dos"}
-        ];
+        ]
     } else {
         return JSON.parse(window.localStorage.getItem("toDos"));
     }
@@ -12,8 +12,9 @@ let toDos = function () {
 
 // THESE TWO FUNCTIONS CREATE THE HTML FOR THE COLUMNS
 function createHTML(toDo) {
-    return '<div class= "col-12 col-lg-8 mx-auto g-3 item"><div><h2>' + toDo.title + '</h2></div></div>';
+    return '<div class= "col-12 col-lg-8 d-flex justify-content-between mx-auto g-3 item"><div><h2 class="title">' + toDo.title + '</h2></div><button class="delete">delete</button></div>';
 }
+
 
 function createColumns(toDos) {
     let html = '';
@@ -24,6 +25,8 @@ function createColumns(toDos) {
 }
 
 // THIS IS TO ADD A TO-DO
+let newTitle = document.querySelector('#inlineFormInputName2');
+
 function addToDo() {
     let newToDo = {};
     newToDo.title = newTitle.value;
@@ -37,15 +40,42 @@ function addToDo() {
 // CLEARS TO-DOs FROM LOCAL STORAGE
 function clearAll() {
     window.localStorage.removeItem('toDos');
-    location.reload();
+    contentArea.innerHTML = createColumns(toDos);
 }
 
+function markDone() {
+    let done = document.getElementsByClassName('item');
+}
 
-// GRABS NEW TO DO
-let newTitle = document.querySelector('#inlineFormInputName2');
 
 // RENDERS THE CONTENT AREA
 let contentArea = document.getElementById('to-do-row');
 contentArea.innerHTML = createColumns(toDos);
 
 
+// THIS CAPTURES ALL THE DELETE BUTTONS AS THEY RENDER
+let readyToDelete = document.getElementsByClassName("delete");
+console.log(readyToDelete);
+for (let i = 0; i < readyToDelete.length; i++) {
+    readyToDelete[i].addEventListener('click', function (e) {
+        e.target.closest('div.item').remove();
+        let index = e.target.closest('div.item').children[0].children[0];
+        // console.log(typeof (index.innerHTML.toLowerCase()));
+        let itemTitle = index.innerHTML.toLowerCase();
+        deleteItem(itemTitle);
+    })
+}
+
+// THIS DELETES AN ITEM
+function deleteItem(item) {
+    console.log(toDos);
+    for (let i = 0; i < toDos.length; i++) {
+        if (item === toDos[i].title) {
+            console.log(i);
+            toDos = toDos.filter(toDo => toDo.title !== item);
+            console.log(toDos);
+        }
+    }
+}
+
+// END OF FUNCTIONS
