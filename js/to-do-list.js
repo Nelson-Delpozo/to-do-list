cda7367b7ddc5a7a9577521c66150b7334ea77b5
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-01-09 18:43:52 -0600

    add a to-do working

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -2,15 +2,25 @@
 
 let toDos = function () {
 
-        if (window.localStorage.getItem("toDos") === null) {
-            return [
-                {title: "No To-Dos"}
-            ];
-        } else {
-            return JSON.parse(window.localStorage.getItem("toDos"));
-        }
-    }();
+    if (window.localStorage.getItem("toDos") === null) {
+        return [
+            {title: "No To-Dos"}
+        ];
+    } else {
+        return JSON.parse(window.localStorage.getItem("toDos"));
+    }
+}();
 
 
-let list = document.getElementById('to-do-row');
-list.innerHTML = '<div class= "col-lg-6 mx-auto item">' + toDos[0].title + '</div>'
+function addToDo() {
+    let newToDo = {};
+    newToDo.title = newTitle.value;
+    toDos.push(newToDo);
+    window.localStorage.setItem('toDos', JSON.stringify(toDos));
+    location.reload();
+}
+
+
+let newTitle = document.querySelector('#inlineFormInputName2');
+
+
