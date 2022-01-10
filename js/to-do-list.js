f1bf154b3ac5e7f6a14f77a96280d4fc22b34f7e
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-01-09 22:43:23 -0600

    almost there

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,4 +1,6 @@
 "use strict";
+let newTitle = document.querySelector('#inlineFormInputName2');
+newTitle.focus();
 
 let toDos = function () {
     if (window.localStorage.getItem("toDos") === null) {
@@ -10,9 +12,14 @@ let toDos = function () {
     }
 }();
 
+// let toDos = [
+//     {title: 'fix fridge'},
+//     {title: 'wash sheets'}
+// ]
+
 // THESE TWO FUNCTIONS CREATE THE HTML FOR THE COLUMNS
 function createHTML(toDo) {
-    return '<div class= "col-12 col-lg-8 d-flex justify-content-between mx-auto g-3 item"><div><h2 class="title">' + toDo.title + '</h2></div><button class="delete">delete</button></div>';
+    return '<div class= "col-12 col-lg-8 d-flex justify-content-between mx-auto g-3 item"><div class="text"><h2 class="title">' + toDo.title + '</h2></div><button class="delete">delete</button></div>';
 }
 
 
@@ -25,7 +32,7 @@ function createColumns(toDos) {
 }
 
 // THIS IS TO ADD A TO-DO
-let newTitle = document.querySelector('#inlineFormInputName2');
+// let newTitle = document.querySelector('#inlineFormInputName2');
 
 function addToDo() {
     let newToDo = {};
@@ -35,12 +42,13 @@ function addToDo() {
     console.log(toDos);
     window.localStorage.setItem('toDos', JSON.stringify(toDos));
     location.reload();
+
 }
 
 // CLEARS TO-DOs FROM LOCAL STORAGE
 function clearAll() {
-    window.localStorage.removeItem('toDos');
-    contentArea.innerHTML = createColumns(toDos);
+    window.localStorage.clear();
+    location.reload();
 }
 
 function markDone() {
@@ -55,7 +63,7 @@ contentArea.innerHTML = createColumns(toDos);
 
 // THIS CAPTURES ALL THE DELETE BUTTONS AS THEY RENDER
 let readyToDelete = document.getElementsByClassName("delete");
-console.log(readyToDelete);
+// console.log(readyToDelete);
 for (let i = 0; i < readyToDelete.length; i++) {
     readyToDelete[i].addEventListener('click', function (e) {
         e.target.closest('div.item').remove();
@@ -66,6 +74,17 @@ for (let i = 0; i < readyToDelete.length; i++) {
     })
 }
 
+// THIS IS FOR MARKING EACH ITEM DONE
+let doneItems = document.getElementsByClassName("item");
+for (let j = 0; j < doneItems.length; j++) {
+    doneItems[j].addEventListener('click', function (e) {
+        let doneItem = e.target.closest('div.item').children[0];
+        console.log(doneItem)
+        doneItem.classList.toggle('done');
+
+    })
+}
+
 // THIS DELETES AN ITEM
 function deleteItem(item) {
     console.log(toDos);
@@ -73,9 +92,11 @@ function deleteItem(item) {
         if (item === toDos[i].title) {
             console.log(i);
             toDos = toDos.filter(toDo => toDo.title !== item);
+
             console.log(toDos);
         }
-    }
+    }window.localStorage.setItem("toDos", JSON.stringify(toDos));
 }
+
 
 // END OF FUNCTIONS
